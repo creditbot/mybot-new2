1fc0fef2fce0e653dc158a7a83ebb86b1489705f
creditbot <44919914+creditbot@users.noreply.github.com>
2018-11-24 18:13:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3299,34 +3299,77 @@ message.channel.sendFile(canvas.toBuffer())
   
   
   
-  client.on('message', message => {
-  if(message.content == '!menbans'){
-      message.guild.fetchBans().then(bans => {
-          bans.forEach(user => {
-             message.channel.send('\`#\` <@'+ user.id + '>');
-          });
-      });
-  }
-});
 
+  client.on('message', message => {
+  var prefix = "!";
+
+           if (message.content === prefix + "date") {
+           if (!message.channel.guild) return message.reply('** This command only for servers **');  
+           var currentTime = new Date(),
+              hours = currentTime.getHours() + 4 ,
+              hours2 = currentTime.getHours() + 3 ,
+              hours3 = currentTime.getHours() + 2 ,
+              hours4 = currentTime.getHours() + 3 ,
+              minutes = currentTime.getMinutes(),
+              seconds = currentTime.getSeconds(),
+              Year = currentTime.getFullYear(),
+              Month = currentTime.getMonth() + 1,
+              Day = currentTime.getDate();
+               var h = hours
+    if(hours > 12) {
+                 hours -= 12;
+              } else if(hours == 0) {
+                  hours = "12";
+              }  
+               if(hours2 > 12) {
+                 hours2 -= 12;
+              } else if(hours2 == 0) {
+                  hours2 = "12";
+               
+              }  
+  if(hours3 > 12) {
+                 hours3 -= 12;
+              } else if(hours3 == 0) {
+                  hours3 = "12";
+              } 
+              if (minutes < 10) {
+                  minutes = '0' + minutes;
+              }
+   
+   
+              var suffix = 'صباحاَ';
+              if (hours >= 12) {
+                  suffix = 'مساء';
+                  hours = hours - 12;
+              }
+              if (hours == 0) {
+                  hours = 12;
+              }
+    
+   
+                  var Date15= new Discord.RichEmbed()
+                  .setThumbnail("https://i.imgur.com/ib3n4Hq.png") 
+                  .setTitle( "「التاريخ  والوقت」")
+                  .setColor('RANDOM')
+                  .setFooter(message.author.username, message.author.avatarURL)
+                  .addField('الامارات',
+                  "「"+ hours + ":" + minutes +":"+ seconds + "」")
+                   .addField('مكه المكرمه',
+                  "「"+ hours2 + ":" + minutes +":"+ seconds  + "」") 
+                  .addField('مصر',
+                  "「"+ hours3 + ":" + minutes +":"+ seconds  + "」") 
+                   
+                  .addField('Date',
+                  "「"+ Day + "-" + Month + "-" + Year +  "」")
+   
+                   message.channel.sendEmbed(Date15);
+          }
+      });
 	
 	
 	
-	client.on('message' , async message => {
-  if(message.author.bot) return;
-  var prefix = "!";     
-  if (message.content.startsWith(prefix + "voice")) {
-  var guild = message.guild
-if(message.channel.type == 'dm') return;
-const embed = new Discord.RichEmbed() 
-    .setColor("RANDOM")
-    .setFooter('Requested by '+message.author.username, message.author.avatarURL)
-    .setAuthor("Voice Online", "https://cdn.discordapp.com/attachments/497456599602823168/502448369663475713/8a1849976873db1d_1.png")
-        .setFooter(` العدد : ${guild.members.filter(member => member.voiceChannel).size}`)
-    .setDescription(`\n${guild.members.filter(member => member.voiceChannel).map(m => m.user.tag).join('\n')}`);
-  message.channel.sendEmbed(embed);
-}// لو تبي تغير الوقت اللي تنرسل فيه الرساله غير رقم 30 الى عدد الدقائق اللي تبيهه ..
-});
+	
+	
 	
 	
 
