6b08f3a932ba516ca74ec635dbabd9350b2b6845
creditbot <44919914+creditbot@users.noreply.github.com>
2018-11-24 14:59:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2686,45 +2686,74 @@ client.on('roleCreate', role => {
 
 
 
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
+  if (message.content.toLowerCase().startsWith(prefix + `help`)) {
+    const embed = new Discord.RichEmbed()
+    .setTitle(`:mailbox_with_mail: Vulnix Help`)
+    .setColor(0xCF40FA)
+    .setDescription(`Hello! I'm Vulnix, the Discord bot for super cool support ticket stuff and more! Here are my commands:`)
+    .addField(`Tickets`, `[${prefix}new]() > Opens up a new ticket and tags the Support Team\n[${prefix}close]() > Closes a ticket that has been resolved or been opened by accident`)
+    .addField(`Other`, `[${prefix}help]() > Shows you this help menu your reading\n[${prefix}ping]() > Pings the bot to see how long it takes to react\n[${prefix}about]() > Tells you all about Vulnix`)
+    message.channel.send({ embed: embed });
+  }
 
+  if (message.content.toLowerCase().startsWith(prefix + `ping`)) {
+    message.channel.send(`Hoold on!`).then(m => {
+    m.edit(`:ping_pong: Wew, made it over the ~waves~ ! **Pong!**\nMessage edit time is ` + (m.createdTimestamp - message.createdTimestamp) + `ms, Discord API heartbeat is ` + Math.round(client.ping) + `ms.`);
+    });
+}
 
-if (message.content.startsWith(`${prefix}rmc`))
-    {
-let mentions = message.content.split(" ").slice(1,2).join(" ")
-        var members = []
-        const linkreg = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi
-        if (members.includes(message.author.id)) return message.reply(`Please wait for reporting another user.`)
-        let evidence = message.content.split(" ").slice(2,3).join(" ")
-        let reason = message.content.split(" ").slice(3).join(" ")
-        if (!mentions) return message.reply(`**f!rmc [IGN] [Evidence URL] [Reason/Message]**`)
-        if (!reason) return message.reply(`**f!rmc [IGN] [Evidence URL] [Reason/Message]**`)
-        if(!evidence.match(linkreg)) return message.channel.send("**f!rmc [IGN] [Evidence URL] [Reason/Message]**")
-        if(!evidence) return message.reply(`Please include evidence in your report.`)
-        var MojangAPI = require('mojang-api');
-var date = new Date();
-date.setMonth(0); // 0 = January
-MojangAPI.uuidAt(mentions, date, function(err, res) {
-    if (err)
-        return message.channel.send(":mag_right: | **We couldn't found " + "``" + `${mentions}` + "``**")
-    else {
-        let namemc = `https://namemc.com/profile/${res.id}`
-        var embed = new Discord.RichEmbed()
-            .setThumbnail("https://d1u5p3l4wpay3k.cloudfront.net/minecraft_de_gamepedia/7/7c/Grasblock.png")
-            .setTitle(`Minecraft Report - ${mentions}`)
-            .setURL(namemc)
-            .setDescription(`**<@!${message.author.id}> has reported ${mentions}**`)
-            .addField(`Reason`, `\`\`${reason}\`\` `)
-            .addField(`Evidence`, evidence)
-            .setColor(`GREEN`)
-            client.channels.get("435156627558105128").send(embed)
-            members.push(message.author.id);
-            message.channel.send(`${mentions} has been reported.`)
-        }
+if (message.content.toLowerCase().startsWith(prefix + `new`)) {
+    const reason = message.content.split(" ").slice(1).join(" ");
+    if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
+    if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
+    message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
+        let role = message.guild.roles.find("name", "Support Team");
+        let role2 = message.guild.roles.find("name", "@everyone");
+        c.overwritePermissions(role, {
+            SEND_MESSAGES: true,
+            READ_MESSAGES: true
         });
-    }
+        c.overwritePermissions(role2, {
+            SEND_MESSAGES: false,
+            READ_MESSAGES: false
+        });
+        c.overwritePermissions(message.author, {
+            SEND_MESSAGES: true,
+            READ_MESSAGES: true
+        });
+        message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
+        const embed = new Discord.RichEmbed()
+        .setColor(0xCF40FA)
+        .addField(`Hey ${message.author.username}!`, `Please try explain why you opened this ticket with as much detail as possible. Our **Support Team** will be here soon to help.`)
+        .setTimestamp();
+        c.send({ embed: embed });
+    }).catch(console.error);
+}
+if (message.content.toLowerCase().startsWith(prefix + `close`)) {
+    if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
 
+    message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`-confirm\`. This will time out in 10 seconds and be cancelled.`)
+    .then((m) => {
+      message.channel.awaitMessages(response => response.content === '-confirm', {
+        max: 1,
+        time: 10000,
+        errors: ['time'],
+      })
+      .then((collected) => {
+          message.channel.delete();
+        })
+        .catch(() => {
+          m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
+              m2.delete();
+          }, 3000);
+        });
+    });
+}
 
+});
 
 
 
