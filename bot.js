66202ed8b6aec794d888e933468909c9f5011843
creditbot <44919914+creditbot@users.noreply.github.com>
2018-11-24 18:33:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3371,7 +3371,57 @@ message.channel.sendFile(canvas.toBuffer())
 	
 	
 	
-	
+	client.on('message', message => {
+    if(message.content == prefix + 'slist') {
+             if(!message.author.id === '323885452207587329') return;
+             if(!message.author.id === '334435543851204618') return;
+    var gimg;
+    var gname;
+    var gmemb;
+    var gbots;
+    var groles;
+    var servers = client.guilds;
+    servers.forEach((g)=>{
+    gname = g.name;
+    gimg = g.iconURL;
+    gmemb = g.members.size;
+    let serv = new Discord.RichEmbed()
+    .setAuthor(gname,gimg)
+    .setThumbnail(gimg)
+    .addField('Server Member Count',gmemb = g.members.size)
+    .setColor('RANDOM')
+    message.channel.send(`
+    
+            `);
+          message.channel.sendEmbed(serv);
+    }) 
+    }
+    });
+    
+    client.on('message', message => {
+    if(message.content == prefix + 'slis') {
+             if(!message.author.id === '323885452207587329') return;
+             if(!message.author.id === '334435543851204618') return;
+    var gimg;
+    var gname;
+    var gmemb;
+    var gbots;
+    var groles;
+    var servers = client.guilds;
+    servers.forEach((g)=>{
+    gname = g.name;
+    gimg = g.iconURL;
+    gmemb = g.members.size;
+    let serv = new Discord.RichEmbed()
+    message.channel.send(`
+    **-------------------------**
+      Server Name : **${gname}**
+      Server MemberCount : **${gmemb} **
+      **---------------------------**
+            `);
+    }) 
+    }
+    });
 	
 	
 	
