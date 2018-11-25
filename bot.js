ca7e7db20153d47383e2ccf169bbe4187fabf31a
creditbot <44919914+creditbot@users.noreply.github.com>
2018-11-25 14:40:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4085,6 +4085,66 @@ DEL.channel.sendEmbed(embed);
 
 
 
+
+client.on('message', message => {
+  if (message.author.bot) return;
+
+
+if(!message.channel.guild) return;
+
+if (!points[message.author.id]) points[message.author.id] = {
+	points: 0,
+  wins: 0,
+  loses: 0,
+  };
+if (message.content.startsWith(prefix + 'انمي')) {
+
+const type = require('./anime.json');
+const item = type[Math.floor(Math.random() * type.length)];
+const filter = response => {
+    return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
+};
+message.channel.send('**لديك 10  ثواني لتخمن الانمي**').then(msg => {
+
+   const embed = new Discord.RichEmbed()
+ .setColor("RANDOM")
+    .setAuthor(`${message.author.tag}`, message.author.avatarURL)
+ .setThumbnail(message.author.avatarURL)     
+ .addField(`**Dark-bot**`,` **${item.type}**`)
+ .setFooter(`ستكسب 15 نقطة`)
+
+msg.channel.send(embed).then(() => {
+        message.channel.awaitMessages(filter, { maxMatches: 1, time: 10000, errors: ['time'] })
+        .then((collected) => {
+		message.channel.send(`**${ collected.first().author} مبروك لقد كسبت 15 نقطة 
+لمعرفة نقاطك الرجاء كتابة *point**` , '');
+		console.log(`[Typing] ${collected.first().author} typed the word.`);
+			let userData = points[collected.first().author.id];
+userData.wins += 1 
+userData.points += 15; 
+
+          })
+
+          .catch(collected => {
+points[message.author.id].loses += 1;
+
+            message.channel.send(`:x: ** الاجابه الصحيحه هي : __${item.answers}__حظ اوفر المرة القادمة ! لقد خسرت , انتهى الوقت**` , '');
+			console.log('[Typing] Error: No one type the word.');
+
+		})
+	})
+    })
+points[message.author.id].game += 1; 
+
+
+}
+fs.writeFile("./point.jsmn",JSON.stringify(points), function(err){
+    if (err) console.log(err);
+  })
+});
+
+
+
 	
 
 client.login(process.env.BOT_TOKEN)
