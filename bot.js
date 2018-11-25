9c8fff1da5c937f9570807cb198950171fbe1c79
creditbot <44919914+creditbot@users.noreply.github.com>
2018-11-25 15:01:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4094,7 +4094,34 @@ DEL.channel.sendEmbed(embed);
 
 
 
+client.on('message',  (message) => {
+        if(message.content.startsWith('!ابلع')) {
+  let user = message.mentions.users.first();
+  if (!user) {
 
+    return message.emit('commandUsage', message, this.help);
+  }
+  let slaps = [
+    'https://i.giphy.com/media/3XlEk2RxPS1m8/giphy.gif',
+    'https://i.giphy.com/media/mEtSQlxqBtWWA/giphy.gif',
+    'https://i.giphy.com/media/j3iGKfXRKlLqw/giphy.gif',
+    'https://i.giphy.com/media/2M2RtPm8T2kOQ/giphy.gif',
+    'https://i.giphy.com/media/l3YSimA8CV1k41b1u/giphy.gif',
+    'https://i.giphy.com/media/WLXO8OZmq0JK8/giphy.gif'
+  ];
+
+  message.channel.send({
+    embed: {
+      description: `${message.author.username} صكك كف ولا عمر ابوك ما ذقته ${user.username}!`,
+      image: {
+        url: slaps[Math.floor(Math.random() * slaps.length)]
+      }
+    }
+  }).catch(e => {
+    client.log.error(e);
+  })
+        }  
+});
 
 
 
